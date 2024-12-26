5a122079ee68de18ca25e223dd8327e04710aaf1
Lonny <lonnymi971@gmail.com>
2024-12-26 22:57:42 +0100

    Initial commit - Site e-commerce

--- app/Utils/Database.php
+++ app/Utils/Database.php
@@ -1,48 +1,21 @@
 <?php
 
-// Retenir son utilisation  => Database::getPDO()
-// Design Pattern : Singleton
-/**
- * Classe qui va nous permettre de nous connecter à notre base de données = oshop
- */
 namespace App\Utils;
 
 use PDO;
 
 class Database
 {
-    /** @var PDO */
-    private $dbh;
-    private static $_instance;
-    private function __construct()
-    {
-        // Récupération des données du fichier de config
-        // la fonction parse_ini_file parse le fichier et retourne un array associatif
-        $configData = parse_ini_file(__DIR__ . '/../config.ini');
+    private static $dbh;
 
-        try {
-            $this->dbh = new PDO(
-                "mysql:host={$configData['DB_HOST']};dbname={$configData['DB_NAME']};charset=utf8",
-                $configData['DB_USERNAME'],
-                $configData['DB_PASSWORD'],
-                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING) // Affiche les erreurs SQL à l'écran
-            );
-        } catch (\Exception $exception) {
-            echo 'Erreur de connexion...<br>';
-            echo $exception->getMessage() . '<br>';
-            echo '<pre>';
-            echo $exception->getTraceAsString();
-            echo '</pre>';
-            exit;
-        }
-    }
-    // the unique method you need to use
     public static function getPDO()
     {
-        // If no instance => create one
-        if (empty(self::$_instance)) {
-            self::$_instance = new Database();
+        if (self::$dbh === null) {
+            $dsn = 'mysql:host=' . CONFIG['database']['DB_HOST'] . ';dbname=' . CONFIG['database']['DB_NAME'] . ';charset=utf8;port=' . CONFIG['database']['DB_PORT'];
+            self::$dbh = new PDO($dsn, CONFIG['database']['DB_USER'], CONFIG['database']['DB_PASSWORD'], [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
         }
-        return self::$_instance->dbh;
+        return self::$dbh;
     }
 }
--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,38 +1,22 @@
-<section>
-    <div class="container-fluid">
-      
+<?php
+?>
+<h2>Bienvenue sur Vinyl Collector</h2>
+<p>Découvrez nos catégories phares :</p>
 
-      <div class="row mx-0">
-        <?php foreach(array_slice($viewData['categories'], 0, 2) as $category) : ?>
-        <div class="col-md-6">
-          <div class="card border-0 text-white text-center"><img src="<?= $category->getPicture() ?>"
-              alt="Card image" class="card-img">
-            <div class="card-img-overlay d-flex align-items-center">
-              <div class="w-100 py-3">
-                <h2 class="display-3 font-weight-bold mb-4"><?= $category->getName() ?></h2><a href="<?=$router->generate('catalog-category', ["id" => $category->getId()])?>" class="btn btn-light"><?=$category->getSubtitle()?></a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <?php endforeach ?>
-
-
-
-      <div class="row mx-0">
-      <?php foreach(array_slice($viewData['categories'], 2, 3) as $category) : ?>
-        <div class="col-lg-4">
-          <div class="card border-0 text-center text-white"><img src="<?= $category->getPicture() ?>"
-              alt="Card image" class="card-img">
-            <div class="card-img-overlay d-flex align-items-center">
-              <div class="w-100">
-                <h2 class="display-4 mb-4"><?= $category->getName() ?></h2><a href="<?=$router->generate('catalog-category', ['id' => $category->getId()])?>" class="btn btn-link text-white"><?=$category->getSubtitle()?>
-                  <i class="fa-arrow-right fa ml-2"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <?php endforeach ?>
-        
-      </div>
+<div class="categories-container">
+    <div class="category-card">
+        <img src="/assets/images/rock.jpg" alt="Rock">
+        <h3>Rock</h3>
+        <a href="/category?id=1">Voir les produits</a>
     </div>
-  </section>
+    <div class="category-card">
+        <img src="/assets/images/jazz.jpg" alt="Jazz">
+        <h3>Jazz</h3>
+        <a href="/category?id=2">Voir les produits</a>
+    </div>
+    <div class="category-card">
+        <img src="/assets/images/classical.jpg" alt="Classique">
+        <h3>Classique</h3>
+        <a href="/category?id=3">Voir les produits</a>
+    </div>
+</div>
